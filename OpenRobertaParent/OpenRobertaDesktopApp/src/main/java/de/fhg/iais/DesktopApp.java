54ba590ca275524e04e29db583dc83e2eeb7a947
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2018-06-14 15:03:29 +0530

    Create seprate classes for Server and DesktopApp to run

--- OpenRobertaParent/OpenRobertaDesktopApp/src/main/java/de/fhg/iais/DesktopApp.java
+++ OpenRobertaParent/OpenRobertaDesktopApp/src/main/java/de/fhg/iais/DesktopApp.java
@@ -1,45 +1,13 @@
 package de.fhg.iais;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.*;
-
-import org.panda_lang.pandomium.Pandomium;
-import org.panda_lang.pandomium.settings.PandomiumSettings;
-import org.panda_lang.pandomium.wrapper.PandomiumBrowser;
-import org.panda_lang.pandomium.wrapper.PandomiumClient;
-
-import de.fhg.iais.roberta.main.ServerStarter;
-
 public class DesktopApp {
-    public static void main(String[] args) throws Exception{
-//        ServerStarter.main(args);
-        
-        PandomiumSettings settings = PandomiumSettings.getDefaultSettings();
-        
-        Pandomium pandomium = new Pandomium(settings);
-        pandomium.initialize();
-
-        PandomiumClient client = pandomium.createClient();
-        PandomiumBrowser browser = client.loadURL("localhost:1999");
-
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(browser.toAWTComponent(), BorderLayout.CENTER);
-
-        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                pandomium.dispose();
-                frame.dispose();
-            }
-        });
-
-        frame.setTitle("OpenRobertaDesktopApp");
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setUndecorated(true);
-        frame.setVisible(true);
+    public static void main(String[] args) throws InterruptedException {
+        Runnable server = new RunServer();
+        Runnable app = new RunDesktopApp();
+        Thread serverThread = new Thread(server);
+        Thread appThread = new Thread(app);
+        serverThread.start();
+        Thread.sleep(10000);
+        appThread.start();
     }
 }
